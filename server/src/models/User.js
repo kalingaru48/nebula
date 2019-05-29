649a49213b6f35ab0d75d1ff63110f22f8e024f4
Ranasinghe Arachchilage Kalinga <inbaykalinga@gmail.com>
2019-05-29 10:15:03 +0100

    Teting database integration

---
+++ server/src/models/User.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+    sequelize.define('User', {
+        email: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            
+        }
+    })
+}
